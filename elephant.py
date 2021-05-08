c94c2789e4a38f72e434ddcc7f7e433d5d7605a5
vedant pol <polvedant7@gmail.com>
2021-05-08 17:47:44 +0530

    Elephant codeforces #340(div2)
    An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
    
    Input
    The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
    
    Output
    Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
    
    Examples
    inputCopy
    5
    outputCopy
    1
    inputCopy
    12
    outputCopy
    3

---
+++ elephant.py
@@ -0,0 +1,20 @@
+x=int(input())
+n=0
+while x!=0:
+    if x>=5:
+        x=x-5
+        n=n+1
+    elif x>=4:
+        x=x-4
+        n=n+1
+    elif x>=3:
+        x=x-3
+        n=n+1
+    elif x>=2:
+        x=x-2
+        n=n+1
+    elif x>=1:
+        x=x-1
+        n=n+1
+print(int(n))
+
