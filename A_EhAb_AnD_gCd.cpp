72111a8835fec6f2feb4bc052745c325d0635932
vedant pol <polvedant7@gmail.com>
2021-06-20 14:31:29 +0530

    EhAb AnD gCd Codeforces Round #628 (Div. 2)
    You are given a positive integer x. Find any such 2 positive integers a and b such that GCD(a,b)+LCM(a,b)=x.
    
    As a reminder, GCD(a,b) is the greatest integer that divides both a and b. Similarly, LCM(a,b) is the smallest integer such that both a and b divide it.
    
    It's guaranteed that the solution always exists. If there are several such pairs (a,b), you can output any of them.
    
    Input
    The first line contains a single integer t (1≤t≤100)  — the number of testcases.
    
    Each testcase consists of one line containing a single integer, x (2≤x≤109).
    
    Output
    For each testcase, output a pair of positive integers a and b (1≤a,b≤109) such that GCD(a,b)+LCM(a,b)=x. It's guaranteed that the solution always exists. If there are several such pairs (a,b), you can output any of them.
    
    Example
    inputCopy
    2
    2
    14
    outputCopy
    1 1
    6 4
    Note
    In the first testcase of the sample, GCD(1,1)+LCM(1,1)=1+1=2.
    
    In the second testcase of the sample, GCD(6,4)+LCM(6,4)=2+12=14.

---
+++ A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{int a;
+cin>>a;
+while(a--){
+    int x;
+        scanf("%d",&x);
+        printf("1 %d\n",x-1);
+
+
+
+}
+return 0;
+}
